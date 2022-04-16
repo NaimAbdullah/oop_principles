0d5b2391544b8ab28c525792cccf7f49cf4b8db1
NaimAbdullah <nabdullah0389@gmail.com>
2022-04-16 12:00:43 -0500

    Added company practice

---
+++ src/encapsulation/Company.java
@@ -0,0 +1,45 @@
+package encapsulation;
+
+public class Company {
+
+    //Create instance variables for name, address, phone number
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    //Override toString method here
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
